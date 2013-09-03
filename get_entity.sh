b5bec2999035010ac27d893d44761e10b594c927
dman777 <darin.hensley@gmail.com>
2013-09-03 18:51:28 -0500

    script for getting entity of server

---
+++ get_entity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+value="label=$(hostname)"
+entity=""
+while read -r line; do
+   if [[ $line =~ $value ]]; then
+      entity="${line:12:10}"
+   fi
+done < <(/usr/local/bin/raxmon-entities-list)
+
+echo "$entity" > /home/entity_file.txt
