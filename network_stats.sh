31b68e75d2c29854370c79f6030cc86036c79d54
dman777 <darin.hensley@gmail.com>
2013-11-18 14:44:12 -0600

    Collects network statistics to trouble shoot networking issues

---
+++ network_stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+##This script is intended to collect network statistics. It is adivisable to 
+##remotley run this as a cron job for the time window you are experiencing 
+#network issues on your remote server.
+
+##CHANGE output_dir to /what_you_prefer if you do not want file going to /home" 
+output_dir="$HOME" 
+##CHANGE destination IP address to what you prefer 
+ip="166.78.155.210" 
+{ 
+echo -e "\n--------------------------------" $(date +%H:%M:%S) "------------------------------------" 
+traceroute "$ip" 
+ping -c 100 "$ip" 
+} >>"$output_dir"/net_tests-$(date +%F).txt 2>&1
