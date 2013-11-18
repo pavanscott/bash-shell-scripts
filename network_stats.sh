ed024b1da291cc94fe9c59892e404de86602c271
dman777 <darin.hensley@gmail.com>
2013-11-18 14:45:30 -0600

    Collects network statistics to trouble shoot networking issues

--- network_stats.sh
+++ network_stats.sh
@@ -3,7 +3,8 @@
 ##remotley run this as a cron job for the time window you are experiencing 
 #network issues on your remote server.
 
-##CHANGE output_dir to /what_you_prefer if you do not want file going to /home" 
+##CHANGE output_dir to /what_you_prefer if you do not want log(s) going to 
+##home directory. 
 output_dir="$HOME" 
 ##CHANGE destination IP address to what you prefer 
 ip="166.78.155.210" 
