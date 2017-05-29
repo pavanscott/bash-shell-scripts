78fb52d920fc11850032b49fdb476341e5b99147
dman777 <darin.hensley@gmail.com>
2017-05-29 06:19:04 +0000

    --allow-empty-message

--- memory_check.sh
+++ memory_check.sh
@@ -1,21 +1,20 @@
 
 #!/bin/bash
 email () {
-/usr/bin/sendmail -nv $EMAIL <<EOF
-From: resume-site@darin.com
-Subject: Memory Alert on Resume Site!
+/usr/sbin/sendmail $EMAIL <<EOF
+  From: resume-site@darin.com
+  Subject: Memory Alert on Resume Site!
 
-Resume site is at $1 MB! 
+  Resume site has $1 MB free! 
 EOF
 }
 
-MEMORY_THRESHOLD=200;
+MEMORY_THRESHOLD=180;
 
 check_memory() {
   set $(free -m)
-  echo "${13}"
   memory=$(printf  "%.0f\n" $(echo "${13}"| tr -d 'GMBK'))
-  ((memory > MEMORY_THRESHOLD)) && email $memory
+  ((memory < MEMORY_THRESHOLD)) && email $memory
 }
 
 check_memory
